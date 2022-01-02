4c6fcfeee1e3c98357ac7287ae685a37544fa680
joachmak <joachimmaksim@gmail.com>
2022-01-02 21:03:45 +0100

    Remove br tag

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -113,7 +113,7 @@ export default function Services() {
                                         <path d="M20.91 11.7l-1.25-1.25c-.6-.6-.93-1.4-.93-2.25v-.86L16.01 4.6a5.56 5.56 0 00-3.94-1.64H9l.92.82A6.18 6.18 0 0112 8.4v1.56l2 2h2.47l2.26 1.91" />
                                     </svg>
                                     <Text align="center" style={styles.text}>
-                                        Bygging av terasser, støttemurer, gjerder <br />og trapper, og renovering av gamle
+                                        Bygging av terasser, støttemurer, gjerder og trapper, og renovering av gamle
                                         hagekonstruksjoner
                                     </Text>
                                 </Flex>
